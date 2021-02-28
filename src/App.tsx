9932c7f0322bface381a2b50296f3538c7427e4c
Victor Machado <victor.m@bokio.se>
2021-02-28 22:11:32 +0100

    add stackoverflow logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,29 @@
 import React from "react";
-import "./App.css";
 import { Table } from "./Table/Table";
+import logo from "./assets/logo-stackoverflow.png";
+import { createUseStyles } from "react-jss";
 
-function App() {
+const useStyles = createUseStyles({
+  layout: {
+    padding: 32,
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  },
+});
+
+const App: React.FC = () => {
+  const classes = useStyles();
+
   return (
-    <div className="App">
-      <h1>Welcome to the Ugly Stack Overflow!</h1>
+    <main className={classes.layout}>
+      <h1>
+        Welcome to the Ugly{" "}
+        <img src={logo} width="250" alt="Stack Overflow's logo" />
+      </h1>
       <Table />
-    </div>
+    </main>
   );
-}
+};
 
 export default App;
